1e056cdaf9c71968d4675ba0bceff853064b669b
Chao Tzu-Hsien <danny900714@gmail.com>
2026-06-14 21:50:50 +0800

    Update integration test for ankiweb client

--- src/ankiweb.test.ts
+++ src/ankiweb.test.ts
@@ -1,5 +1,10 @@
+/**
+ * AnkiWeb API client integration tests
+ * @module-tag integration
+ */
+
 import { describe, expect, test } from "vitest";
-import { login, uploadAddon } from "./ankiweb.js";
+import { HTTPStatusError, login, uploadAddon } from "./ankiweb.js";
 
 describe("login", () => {
   test("should return ankiweb cookie", async () => {
@@ -10,16 +15,36 @@ describe("login", () => {
 
 describe("uploadAddonRequest", () => {
   test("upload Anki addon to AnkiWeb", async () => {
-    await uploadAddon(process.env.ANKIWEB_COOKIE, {
-      addonId: 799866239,
-      title: "Test new title for @danny900714/upload-anki-addon",
-      tags: "test new tag",
-      supportUrl: "https://github.com/danny900714/upload-anki-addon",
-      description: "New description for `@danny900714/upload-anki-addon`!",
-      branches: [
-        { minVersion: 1, maxVersion: 66 },
-        { minVersion: 231000, maxVersion: 250904 },
-      ],
-    });
+    const addonFileBase64 =
+      "UEsDBAoAAAAAABWtzlwAAAAAAAAAAAAAAAALABwAX19pbml0X18ucHlVVAkAA1mvLmpZry5qdXgLAAEE9QEAAAQUAAAAUEsBAh4DCgAAAAAAFa3OXAAAAAAAAAAAAAAAAAsAGAAAAAAAAAAAAKSBAAAAAF9faW5pdF9fLnB5VVQFAANZry5qdXgLAAEE9QEAAAQUAAAAUEsFBgAAAAABAAEAUQAAAEUAAAAAAA==";
+
+    try {
+      await uploadAddon(
+        process.env.ANKIWEB_COOKIE,
+        {
+          addonId: process.env.ANKIWEB_ADDON_ID
+            ? parseInt(process.env.ANKIWEB_ADDON_ID, 10)
+            : undefined,
+          title: "Integration test for ankiweb client of @danny900714/upload-anki-addon",
+          tags: "integration test tag",
+          supportUrl: "https://github.com/danny900714/upload-anki-addon",
+          description:
+            "This is the test addon uploaded during integration test for ankiweb client.",
+          branches: [{ minVersion: 1, maxVersion: 250904 }],
+        },
+        {
+          zipData: Buffer.from(addonFileBase64, "base64"),
+          branchIndex: 0,
+        },
+      );
+    } catch (error) {
+      if (error instanceof HTTPStatusError) {
+        console.error(error.response);
+        console.error(await error.response.text());
+      } else {
+        console.error(error);
+      }
+      throw error;
+    }
   });
 });
